38ba8e21ee3617399cc35e77cf627a837c684f83
Naftali Harris <naftaliharris@gmail.com>
2014-02-26 22:23:00 -0800

    Fixed several bugs in reading pages from files

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -191,14 +191,16 @@ page_fault (struct intr_frame *f)
             }
         }
     }
+
   //END LP Poject 3 code
     
-    
-  /*printf ("Page fault at %p: %s error %s page in %s context.\n",
+
+  /* User segfault */    
+  printf ("Page fault at %p: %s error %s page in %s context.\n",
           fault_addr,
           not_present ? "not present" : "rights violation",
           write ? "writing" : "reading",
           user ? "user" : "kernel");
-  kill (f);*/
+  kill (f);
 }
 
