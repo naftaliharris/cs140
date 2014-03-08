fb04b47418dbc0de9520846ac06f2a1602b411f3
Naftali Harris <naftaliharris@gmail.com>
2014-03-07 23:55:47 -0800

    Implemented getting free blocks and writing back all sectors.

--- src/filesys/cache.c
+++ src/filesys/cache.c
@@ -1,69 +1,175 @@
+#include <bitmap.h>
 #include "filesys/cache.h"
 #include "threads/malloc.h"
 #include "filesys/filesys.h"
 
+/* Design goals:
+ * 1) FS cache contains all unique cache blocks, (no repetitions)
+ *      => Must have lock around at least all sectors.
+ * 2) Multiple reads can happen simultaneously
+ * 3) Cache writes occur eventually
+ *      => 2 and 3 solved by rw_lock
+ * 4) Cache periodically written back to disk
+ *      => solved by daemon or in thread_tick
+ * 5) Asyncronous loading of caches
+ *      => solved by daemon
+ * 6) Threads might suddenly die?
+ *      => Not while they are running OS code!
+ */
+
 void
 init_fs_cache(void)
 {
-    /* TODO: Figure out if can remove the lock acquire and release here:
-     * Probably.
-     */
-    lock_init(&fs_cache_lock);
-    lock_acquire(&fs_cache_lock);
+    rw_lock_init(&sector_lock);
+    lock_init(&fs_evict_lock);
+
     fs_cache = (struct cached_block*)malloc(CACHE_BLOCKS * sizeof(struct cached_block));
     if (fs_cache == NULL) {
-        PANIC("Couldn't allocated file system cache!");
+        PANIC("Couldn't allocate file system cache!");
     }
 
     int i;
     for (i = 0; i < CACHE_BLOCKS; i++)
     {
-        lock_init(&fs_cache[i].lock);
-        lock_acquire(&fs_cache[i].lock);
-        fs_cache[i].occupied = false;
-        fs_cache[i].data = malloc(BLOCK_SECTOR_SIZE);
-        if (fs_cache[i].data == NULL) {
-            PANIC("Couldn't allocate file system cache!");
+        struct cached_block *cb = &fs_cache[i];
+        rw_lock_init(&cb->rw_lock);
+        cb->state = UNOCCUPIED;
+        cb->accessed = false;
+        cb->data = malloc(BLOCK_SECTOR_SIZE);
+        if (cb->data == NULL) {
+            PANIC("Couldn't allocate a file system cache buffer!");
         }
-        lock_release(&fs_cache[i].lock);
     }
 
     fs_cache_arm = 0;
-    lock_release(&fs_cache_lock);
 }
 
+/* XXX: Must be called from a write-lock context! */
 void
 write_back(struct cached_block *cb)
 {
-    ASSERT(lock_held_by_current_thread(&cb->lock));
-    if (cb->occupied && cb->dirty) {
-       block_write(fs_device, cb->sector, cb->data);
-        cb->dirty = false;
+    /* The thread that is doing the IO should have the write-lock */
+    ASSERT (cb->state != IN_IO);
+
+    if (cb->state == DIRTY) {
+        cb->state = IN_IO; 
+        block_write(fs_device, cb->sector, cb->data);
+        cb->state = CLEAN;
     }
 }
 
-/* Returns an unoccupied block and acquires its lock. */
+/* Returns an unoccupied block and acquires its lock as a writer */
 struct cached_block *
 get_free_block(void)
 {
-    lock_acquire(&fs_cache_lock);
+    lock_acquire(&fs_evict_lock);
     while(true)
     {
         struct cached_block *cb = &fs_cache[fs_cache_arm];
         fs_cache_arm = (fs_cache_arm + 1) % CACHE_BLOCKS;
-        if (lock_try_acquire(&cb->lock))
-        {
-            if (cb->occupied == false) {
-                lock_release(&fs_cache_lock);
+        if (writer_try_acquire(&cb->rw_lock)) {
+            if (cb->state == UNOCCUPIED) {
+                lock_release(&fs_evict_lock);
                 return cb;
             }
             if (cb->accessed == false) {
-                lock_release(&fs_cache_lock);
+                lock_release(&fs_evict_lock);
                 write_back(cb);
-                cb->occupied = false;
+                cb->state = UNOCCUPIED;
                 return cb;
             }
             cb->accessed = false;
+        }
+    }
+}
+
+
+/* Finds the cached_block, if present.
+ * If the cached_block is present...
+ *
+ *
+ */
+
+//struct cached_block *
+//find_cached_sector(block_sector_t sector)
+//{
+//    reader_lock_acquire(&sector_lock);
+//    int i;
+//    for (i = 0; i < CACHE_BLOCKS; i++)
+//    {
+//        struct cached_block *cb = &fs_cache[i];
+//        if (cb->sector == sector)
+//            return cb;
+//        }
+//    }
+//    return NULL;
+//}
+
+//void
+//cached_read(block_sector_t sector, uint32_t from, uint32_t to, void *buffer)
+//{
+//    ASSERT (0 <= from);
+//    ASSERT (from < to);
+//    ASSERT (to < BLOCK_SECTOR_SIZE);
+//
+//    /* Outline:
+//     * 1) Find the cb, getting a new one if necessary, and acquire read lock
+//     * 2) Read
+//     * 3) Release read lock
+//     */
+//
+//    struct cached_block *cb = find_cached_sector(sector);
+//
+//    if (cb == NULL) {
+//        cb = get_free_block();
+//        cb->occupied = true;
+//        cb->accessed = true;
+//        cb->dirty = false;
+//        cb->being_loaded = true;
+//        lock_release(&cache_state_lock);
+//
+//        block_read(fs_device, sector, cb->data);
+//        lock_acquire(&cache_state_lock);
+//        cb->being_loaded = false;
+//        lock_release(&cache_state_lock);
+//
+//        // Lock or something
+//        // Read or something
+//    } else {
+//        cb->accessed = true;
+//        lock_release(&cache_state_lock);
+//
+//        // Lock or something
+//        // Read or something
+//    }
+//}
+
+//void
+//cached_write(block_sector_t sector, uint32_t from, uint32_t to, void *buffer)
+//{
+//    ASSERT (0 <= from);
+//    ASSERT (from < to);
+//    ASSERT (to < BLOCK_SECTOR_SIZE);
+//
+//}
+//
+
+void
+write_back_all(void)
+{
+    /* XXX: Hard-coded 64 cache blocks */
+    uint64_t wrote_back = 0;
+    int i;
+    while (wrote_back != UINT64_MAX) {
+        for (i = 0; i < 64; i++) {
+            struct cached_block *cb = &fs_cache[i];
+            if (wrote_back & (1 << i) == 0) {
+                if (writer_try_acquire(&cb->rw_lock)) {
+                    write_back(cb);
+                    wrote_back |= (1 << i);
+                    writer_release(&cb->rw_lock);
+                }
+            }
         }
     }
 }
--- src/filesys/cache.h
+++ src/filesys/cache.h
@@ -6,22 +6,43 @@
 
 #define CACHE_BLOCKS 64
 
+#define CACHE_OCCUPIED 0x1
+#define CACHE_ACCESSED 0x2
+#define CACHE_DIRTY 0x4
+#define CACHE_WRITING 0x8
+#define CACHE_PINNED 0x10
+
+typedef enum cached_block_state
+{
+    UNOCCUPIED,
+    IN_IO,
+    CLEAN,
+    DIRTY
+} cb_state_t;
+
 struct cached_block
 {
-    struct lock lock;
-    block_sector_t sector;
-    bool occupied;
-    bool dirty;
+    struct rw_lock rw_lock;  /* Protects the state below */
+
+    block_sector_t sector;    /* Protected by both global sector lock and the per-cached_block rw_lock */
+    cb_state_t state;
     bool accessed;
     void *data;
 };
 
-struct lock fs_cache_lock;
+/* The actual file-system cache */
 struct cached_block *fs_cache;
+
+/* Eviction data */
+struct lock fs_evict_lock; /* Only one cached_block may be evicted at a time */
 int fs_cache_arm;
+
+/* Block sector data */
+struct rw_lock sector_lock; /* Lock for all sectors */
 
 /* Prototypes */
 void init_fs_cache(void);
 void write_back(struct cached_block *cb);
 struct cached_block *get_free_block(void);
+void write_back_all(void);
 #endif
