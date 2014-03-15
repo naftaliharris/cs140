9062c21994da5be22db3c47ed0f35a6df691e379
Luke Marco Pappas <lpappas9@corn09.stanford.edu>
2014-03-15 07:01:48 -0700

    commit of synch.c modification of lock_acquire for debug

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -246,6 +246,9 @@ lock_acquire (struct lock *lock)
 {
     ASSERT (lock != NULL);
     ASSERT (!intr_context ());
+    if (lock_held_by_current_thread (lock)) {
+        printf("break");
+    }
     ASSERT (!lock_held_by_current_thread (lock));
     
     enum intr_level old_level = intr_disable();
